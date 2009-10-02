af15d2480932cddfb27e70696a772620812f03bc
leipzig <leipzig@7f1b7962-90f5-11de-80e4-19beea365de2>
2009-10-02 16:56:41 +0000

    added 1k column, useful for transcript length contigs N50 is actually >=

--- calculateStats.R
+++ calculateStats.R
@@ -1,20 +1,20 @@
 myTable<-read.table(statTab,header=TRUE)
 myMeta<-read.table(metaTab,header=TRUE)
+#http://www.mail-archive.com/r-help@stat.math.ethz.ch/msg74771.html
 library(proto)
 
 myArray<-as.vector(myMeta)
 myArray["groupDir"]<-groupDir
-#bottom filter on contig length is set by default
-#myTable<-myTable[myTable$lgth>(myMeta$kmer-1),]
-
 myArray["ctgs"]<-nrow(myTable)
 myArray["meanLgth"]<-mean(myTable$lgth+myMeta$kmer-1)
 myArray["medianLgth"]<-median(myTable$lgth+myMeta$kmer-1)
 myArray["totalCoverage"]<-sum(myTable$lgth+myMeta$kmer-1)
+myArray["maxLgth"]<-max(myTable$lgth+myMeta$kmer-1)
+myArray["over1k"]<-length(which(myTable$lgth+myMeta$kmer-1>=1000))
 
 #https://stat.ethz.ch/pipermail/r-help/2008-November/180966.html
 x<-rev(sort(myTable$lgth+myMeta$kmer-1))
-myArray["N50"]<-x[cumsum(x) > sum(x)/2][1]
+myArray["N50"]<-x[cumsum(x) >= sum(x)/2][1]
 
 statFileName<-paste(statFile,".frame.RData",sep="")
 if(length(dir(pattern=statFileName))>0){
