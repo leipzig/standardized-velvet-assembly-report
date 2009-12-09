a4fd1635c92cd948427d9c32beafc93106215849
leipzig <leipzig@7f1b7962-90f5-11de-80e4-19beea365de2>
2009-12-09 17:30:54 +0000

    -added more support for gzipped and fastq files -changed the method for calculating read usage to use UnusedReads instead of velvet_asm.afg by default

--- generateAssemblyStats.pl
+++ generateAssemblyStats.pl
@@ -1,114 +1,250 @@
 #!/usr/bin/perl
-
+use strict;
 use PerlIO::gzip;
+use Getopt::Long;
+use Pod::Usage;
 
-$rootDir=$ARGV[0];
+my ($man,$help);
+my $useamos = 0; #slower, will return lower read usage relative to using UnusedReads.fa
 
-opendir(DIR, $rootDir);
-@allfiles=readdir(DIR);
-@blatfiles = grep(/\.psl$/,@allfiles);
-@blastfiles = grep(/m8$/,@allfiles);
+GetOptions('useamos'=>\$useamos,'help|?' => \$help,'man' => \$man) or pod2usage(2);
+pod2usage(1) if ($help || (@ARGV==0));
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+
+my $rootDir = shift @ARGV;
+die("$rootDir is not a directory") unless (-d $rootDir);
+
+opendir( DIR, $rootDir );
+my @allfiles   = readdir(DIR);
+my @blatfiles  = grep( /\.psl$/, @allfiles );
+my @blastfiles = grep( /m8$/, @allfiles );
+my @readsfiles = grep( /^reads.stat/, @allfiles );
 closedir(DIR);
 
-if(scalar(@blatfiles)){
-    $blatFile=$blatfiles[0];
-    open(BLAT,"<$rootDir/$blatFile") or die("can't find blat file");
-    %winners;
-    %lengths;
-    while(<BLAT>){
-    #match   mis-    rep.    N's     Q gap   Q gap   T gap   T gap   strand  Q               Q       Q       Q       T               T       T       T       block   blockSizes      qStarts  tStarts
-    #        match   match           count   bases   count   bases           name            size    start   end     name            size    start   end     count
-    #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-    #45      0       0       0       0       0       0       0       -       NODE_1_length_31_cov_17.741936  45      0       45      AC207475.3      175195  14643   14688   1       45,     0,      14643,
+my ( %winners,  %lengths );                  #from blat or blast
+my ( $alignLen, $alignWin, $uniqueHits );    #from blat or blast
+my ( $kmer,     $cvCut, $expCov );           #from log file
+my ( $totBP,    $goodContigs );              #from contigs file
 
-    	if(
-	   ($match,$query,$qSize)=$_=~/^(\d+)\t\d+\t\d+\t\d+\t\d+\t\d+\t\d+\t\d+\t[-+]\t(\S+)\t(\d+)/
-	   ){
-	   if($match/$qSize>=0.90){
-	  $winners{$query}++;$lengths{$query}=$match
-        }
-       }
-    }
-    close(BLAT);
-}else{
-    if(scalar(@blastfiles)){
-	$blastFile=$blastfiles[0];
-open(BLAST,"<$rootDir/$blastFile") or die("can't find blast file");
-while(<BLAST>){
-        ($query,$lgth,$bit)=$_=~/^(\S+)\t\S+\t\S+\t(\d+).+\t\s?([0-9.]+)$/;
-        if($query ne $oq){
-            $winners{$query}=$bit;
-            $lengths{$query}=$lgth;
-            $ob=$bit;
-            $oq=$query;
-        }elsif($winners{$query}>0 && $bit>0.95*$ob){
-            $winners{$query}=0;
-        }
+my ( $reads, $tiles );    #from either Reads/UnusedReads or velvet_asm.afg AMOS file
+
+if ( scalar(@blatfiles) ) {
+    parseBlat( $blatfiles[0], \%winners, \%lengths );
 }
-close(BLAST);
-    }
+elsif ( scalar(@blastfiles) ) {
+    parseBlast( $blastfiles[0], \%winners, \%lengths );
+}
+if ( scalar(@blatfiles) || scalar(@blastfiles) ) {
+    postAlign( \%winners, \%lengths );
 }
 
+countContigs();
 
-
-
-if(scalar(@blatfiles) || scalar(@blastfiles)){
-#length of all decent blatted queries
-$alignLen=0;
-foreach $q(keys %lengths)
-{$alignLen+=$lengths{$q}}
-
-#any queries that blatted well
-$alignWin=scalar(keys(%winners));
-
-#find distinctive hits
-$uniqueHits=0;
-foreach $h(keys(%winners)){
-    if($winners{$h}==1){$uniqueHits++}
+if ( $useamos == 1 ) {
+    getTilesUsingAMOS();
 }
-}#if blatfile or blastfile
-
-open(CONTIGS,"<$rootDir/contigs.fa");
-$totBP=0;$goodContigs=0;
-while(<CONTIGS>)
-{
-    chomp();
-    if(/>/){$goodContigs++;}
-    else{$totBP+=length($_);}
+else {
+    getTilesUsingReads();
 }
-close(CONTIGS);
-open(LOG,"<$rootDir/Log") or die("can't find log file");
-while($line=<LOG>){
-  print STDERR $line."\n";
 
-  if($line=~/velveth/){
-      ($kmer)=$line=~/ (\d+) /; #kmer usually an isolated entry
-  }
-  if($line=~/velvetg/){
-    ($cvCut) =$line=~/-cov_cutoff ([0-9.]+)/;
-    ($expCov)=$line=~/-exp_cov ([0-9.]+)/;
-  } 
-}
-close(LOG);
+parseLogFile();
 
-
-$reads=0;
-$tiles=0;
-$inTile=0;#AMOS file does not distinguish src as reads in a node from nodes in a contig
-%readSrc;
-open ASM,"<:gzip","$rootDir/velvet_asm.afg.gz" or open ASM,"<$rootDir/velvet_asm.afg" or die "can't find .afg file";
-while(<ASM>){
-    if(/\{RED/){$reads++}
-    elsif(/\{TLE/){$inTile=1;}
-    elsif(/src/){if($inTile){$readSrc{$_}=1;$inTile=0;}}
-    #elsif(/\{CTG/){$contigs++}not reliable
-}
-close(ASM);
-$tiles=scalar keys %readSrc;
-
-print "totBP"."\t"."reads"."\t"."tiles"."\t"."goodContigs"."\t"."kmer"."\t"."cvCut"."\t"."expCov";
-print "\t"."blatHit"."\t"."bpAligned"."\t"."uniqueHits" if(scalar(@blatfiles) || scalar(@blastfiles));
+print join "\t", qw /totBP reads tiles goodContigs kmer cvCut expCov/;
+print "\tblatHit\tbpAligned\tuniqueHits"
+  if ( scalar(@blatfiles) || scalar(@blastfiles) );
 print "\n";
 print "$totBP\t$reads\t$tiles\t$goodContigs\t$kmer\t$cvCut\t$expCov";
-print "\t$alignWin\t$alignLen\t$uniqueHits" if(scalar(@blatfiles) || scalar(@blastfiles));
+print "\t$alignWin\t$alignLen\t$uniqueHits"
+  if ( scalar(@blatfiles) || scalar(@blastfiles) );
 print "\n";
+
+sub parseBlat {
+    my ( $blatFile, $winnersref, $lengthsref ) = @_;
+    open( BLAT, "<$rootDir/$blatFile" ) or die("can't find blat file");
+
+    while (<BLAT>) {
+
+#match   mis-    rep.    N's     Q gap   Q gap   T gap   T gap   strand  Q               Q       Q       Q       T               T       T       T       block   blockSizes      qStarts  tStarts
+#        match   match           count   bases   count   bases           name            size    start   end     name            size    start   end     count
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+#45      0       0       0       0       0       0       0       -       NODE_1_length_31_cov_17.741936  45      0       45      AC207475.3      175195  14643   14688   1       45,     0,      14643,
+
+        if ( my ( $match, $query, $qSize ) =
+            $_ =~
+            /^(\d+)\t\d+\t\d+\t\d+\t\d+\t\d+\t\d+\t\d+\t[-+]\t(\S+)\t(\d+)/ )
+        {
+            if ( $match / $qSize >= 0.90 ) {
+                $winnersref->{$query}++;
+                $lengthsref->{$query} = $match;
+            }
+        }
+    }
+    close(BLAT);
+}
+
+sub parseBlast {
+    my ( $blastFile, $winnersref, $lengthsref ) = @_;
+    open( BLAST, "<$rootDir/$blastFile" ) or die("can't find blast file");
+    my ( $oq, $ob );
+    while (<BLAST>) {
+        my ( $query, $lgth, $bit ) =
+          $_ =~ /^(\S+)\t\S+\t\S+\t(\d+).+\t\s?([0-9.]+)$/;
+        if ( $query ne $oq ) {
+            $winnersref->{$query} = $bit;
+            $lengthsref->{$query} = $lgth;
+            $ob                   = $bit;
+            $oq                   = $query;
+        }
+        elsif ( $winnersref->{$query} > 0 && $bit > 0.95 * $ob ) {
+            $winnersref->{$query} = 0;
+        }
+    }
+    close(BLAST);
+}
+
+sub postAlign {
+
+    #length of all decent blatted queries
+
+    foreach my $q ( keys %lengths ) { $alignLen += $lengths{$q} }
+
+    #any queries that blatted well
+    $alignWin = scalar( keys(%winners) );
+
+    #find distinctive hits
+    foreach my $h ( keys(%winners) ) {
+        if ( $winners{$h} == 1 ) { $uniqueHits++ }
+    }
+}
+
+sub getTilesUsingReads {
+    my $readType;
+    if ( -e "$rootDir/reads.stat" ){
+	open( READS, "<$rootDir/reads.stat" );
+    }else{
+	die("can't find reads.stat file");
+    }
+    while(<READS>)
+    {
+	if(/^(\d+)\t(\d+)$/){
+	    $reads=$1;
+	}
+    }
+
+    if ( -e "$rootDir/UnusedReads.fa" ) {
+        open( UNUSED, "<$rootDir/UnusedReads.fa" );
+    }
+    elsif ( -e "$rootDir/UnusedReads.fa.gz" ) {
+        open( UNUSED, "<:gzip", "$rootDir/UnusedReads.fa.gz" );
+    }
+    else {
+        die("can't find a suitable UnusedReads file");
+    }
+    my ($unusedReads);
+    while (<UNUSED>) {
+        if (/>/) { $unusedReads++; }
+    }
+    close(UNUSED);
+    $tiles = $reads - $unusedReads;
+}
+
+sub getTilesUsingAMOS {
+
+#warning: using the velvet_asm.afg file will deflate your read usage compared to the Log as redundant reads are not credited in the file
+#AMOS file does not distinguish src as reads in a node from nodes in a contig
+    my $inTile;
+
+    my %readSrc;
+
+    if ( -e "$rootDir/velvet_asm.afg" ) {
+        open( ASM, "<$rootDir/velvet_asm.afg" );
+    }
+    elsif ( -e "$rootDir/velvet_asm.afg.gz" ) {
+        open( ASM, "<:gzip", "$rootDir/velvet_asm.afg.gz" );
+    }
+    else {
+        die("can't find a suitable .afg file");
+    }
+
+    while (<ASM>) {
+        if (/\{RED/) { $reads++ }
+        elsif (/\{TLE/) { $inTile = 1; }
+        elsif (/src/) {
+            if ($inTile) { $readSrc{$_} = 1; $inTile = 0; }
+        }
+    }
+    close(ASM);
+    $tiles = scalar keys %readSrc;
+}
+
+sub countContigs {
+    if ( -e "$rootDir/contigs.fa" ) {
+        open( CONTIGS, "<$rootDir/contigs.fa" );
+    }
+    elsif ( -e "$rootDir/contigs.fa.gz" ) {
+        open( CONTIGS, "<:gzip", "$rootDir/contigs.fa.gz" );
+    }
+    else {
+        die("can't find a suitable contigs file");
+    }
+
+    while (<CONTIGS>) {
+        chomp();
+        if   (/>/) { $goodContigs++; }
+        else       { $totBP += length($_); }
+    }
+    close(CONTIGS);
+}
+
+sub parseLogFile {
+    open( LOG, "<$rootDir/Log" ) or die("can't find log file");
+    while ( my $line = <LOG> ) {
+        print STDERR $line . "\n";
+
+        if ( $line =~ /velveth/ ) {
+            ($kmer) = $line =~ / (\d+) /;    #kmer usually an isolated entry
+        }
+        if ( $line =~ /velvetg/ ) {
+            ($cvCut)  = $line =~ /-cov_cutoff ([0-9.]+)/;
+            ($expCov) = $line =~ /-exp_cov ([0-9.]+)/;
+        }
+    }
+    close(LOG);
+}
+
+ __END__
+ 
+=head1 Generate Assembly Stats
+ 
+=head1 SYNOPSIS
+
+generateAssemblyStats.pl -useamos ./out_ecoli_31_38_76_dir
+
+Options:
+  -help
+  -useamos use velvet_asm.afg instead of UnusedReads.fa to compute read usage
+
+=head1 OPTIONS
+
+=over 8
+ 
+=item B<-help>
+ 
+This help message
+
+=back
+
+=over 8
+ 
+=item B<-useamos>
+
+Use velvet_asm.afg instead of UnusedReads.fa to compute read usage. This will make your reports comparable to older versions of SVAR, but it is slower and will return lower read usage relative to using UnusedReads.fa
+
+=back
+
+=head1 DESCRIPTION
+
+Produce a tab-delimited metadata file for Standard Velvet Assembly reports
+
+=cut
+
